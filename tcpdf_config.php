0aef960239e877cb05490fa63ba7953820bcba33
WTellos <31623201+wtellos@users.noreply.github.com>
2025-05-21 12:38:37 +0300

    Update tcpdf_config.php
    Added source/reference

--- tcpdf_config.php
+++ tcpdf_config.php
@@ -1,6 +1,7 @@
 <?php 
 
 // Path to file: /wp-content/plugins/sfwd-lms/includes/lib/tcpdf/config/tcpdf_config.php
+// Source: https://discoverelearning.com/insights/how-to-add-custom-fonts-to-your-learndash-lms-certificates
 <?php
 //============================================================+
 // File name   : tcpdf_config.php
